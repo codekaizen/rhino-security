671af311d1537f19bffcd67b3199a80c31747d58
Rory <rplaire@hotmail.com>
2020-04-23 14:36:45 -0700

    Fixing up mixed tab/space; switching to netstandard

--- Rhino.Security.Tests/DatabaseFixture.cs
+++ Rhino.Security.Tests/DatabaseFixture.cs
@@ -15,105 +15,105 @@ using Environment = NHibernate.Cfg.Environment;
 namespace Rhino.Security.Tests
 {
     public abstract class DatabaseFixture : IDisposable
-	{
-		protected readonly ISessionFactory factory;
-		protected Account account;
-		protected IAuthorizationRepository authorizationRepository;
-		protected IAuthorizationService authorizationService;
-		protected IPermissionsService permissionService;
-		protected IPermissionsBuilderService permissionsBuilderService;
+    {
+        protected readonly ISessionFactory factory;
+        protected Account account;
+        protected IAuthorizationRepository authorizationRepository;
+        protected IAuthorizationService authorizationService;
+        protected IPermissionsService permissionService;
+        protected IPermissionsBuilderService permissionsBuilderService;
 
-		protected ISession session;
-		protected User user;
-	    private static Object sync = new Object();
+        protected ISession session;
+        protected User user;
+        private static Object sync = new Object();
 
-		protected DatabaseFixture()
-		{
-			BeforeSetup();
+        protected DatabaseFixture()
+        {
+            BeforeSetup();
 
             Monitor.Enter(sync);
-			SillyContainer.SessionProvider = (() => session);
-			var sillyContainer = new SillyContainer();
-			ServiceLocator.SetLocatorProvider(() => sillyContainer);
+            SillyContainer.SessionProvider = (() => session);
+            var sillyContainer = new SillyContainer();
+            ServiceLocator.SetLocatorProvider(() => sillyContainer);
 
-			Assert.NotNull(typeof (SQLiteConnection));
+            Assert.NotNull(typeof (SQLiteConnection));
 
-			Configuration cfg = new Configuration()
+            Configuration cfg = new Configuration()
                 .SetProperty(Environment.ConnectionDriver, typeof(SQLite20Driver).AssemblyQualifiedName)
                 .SetProperty(Environment.Dialect, typeof(SQLiteDialect).AssemblyQualifiedName)
                 //.SetProperty(Environment.ConnectionDriver, typeof(Sql2008ClientDriver).AssemblyQualifiedName)
                 //.SetProperty(Environment.Dialect, typeof(MsSql2008Dialect).AssemblyQualifiedName)
                 .SetProperty(Environment.ConnectionString, ConnectionString)
-				//.SetProperty(Environment.ProxyFactoryFactoryClass, typeof(ProxyFactoryFactory).AssemblyQualifiedName)
-				.SetProperty(Environment.ReleaseConnections, "on_close")
-				.SetProperty(Environment.UseSecondLevelCache, "true")
-				.SetProperty(Environment.UseQueryCache, "true")
-				.SetProperty(Environment.CacheProvider, typeof (HashtableCacheProvider).AssemblyQualifiedName)
+                //.SetProperty(Environment.ProxyFactoryFactoryClass, typeof(ProxyFactoryFactory).AssemblyQualifiedName)
+                .SetProperty(Environment.ReleaseConnections, "on_close")
+                .SetProperty(Environment.UseSecondLevelCache, "true")
+                .SetProperty(Environment.UseQueryCache, "true")
+                .SetProperty(Environment.CacheProvider, typeof (HashtableCacheProvider).AssemblyQualifiedName)
                 .SetProperty(Environment.ShowSql, "true")
-				.AddAssembly(typeof (User).Assembly);
+                .AddAssembly(typeof (User).Assembly);
 
-			Security.Configure<User>(cfg, SecurityTableStructure.Prefix);
+            Security.Configure<User>(cfg, SecurityTableStructure.Prefix);
 
-			factory = cfg.BuildSessionFactory();
+            factory = cfg.BuildSessionFactory();
 
-			session = factory.OpenSession();
+            session = factory.OpenSession();
 
-			new SchemaExport(cfg).Execute(true, true, false, session.Connection, null);
+            new SchemaExport(cfg).Execute(true, true, false, session.Connection, null);
 
-			session.BeginTransaction();
+            session.BeginTransaction();
 
-			SetupEntities();
+            SetupEntities();
 
-			session.Flush();
-		}
+            session.Flush();
+        }
 
-		public virtual string ConnectionString
-		{
+        public virtual string ConnectionString
+        {
             get { return "Data Source=:memory:"; }
         }
 
-		#region IDisposable Members
+        #region IDisposable Members
 
-		public virtual void Dispose()
-		{
-		    try
-		    {
-		        if (session.Transaction.IsActive)
-		            session.Transaction.Rollback();
-		        session.Dispose();
+        public virtual void Dispose()
+        {
+            try
+            {
+                if (session.Transaction.IsActive)
+                    session.Transaction.Rollback();
+                session.Dispose();
             }
-		    finally
-		    {
-		        Monitor.Exit(sync);
-		    }
-		}
+            finally
+            {
+                Monitor.Exit(sync);
+            }
+        }
 
-		#endregion
+        #endregion
 
-		protected virtual void BeforeSetup()
-		{
-		}
+        protected virtual void BeforeSetup()
+        {
+        }
 
-		private void SetupEntities()
-		{
-			user = new User {Name = "Ayende"};
-			account = new Account {Name = "south sand"};
+        private void SetupEntities()
+        {
+            user = new User {Name = "Ayende"};
+            account = new Account {Name = "south sand"};
 
-			session.Save(user);
-			session.Save(account);
+            session.Save(user);
+            session.Save(account);
 
-			authorizationService = ServiceLocator.Current.GetInstance<IAuthorizationService>();
-			permissionService = ServiceLocator.Current.GetInstance<IPermissionsService>();
-			permissionsBuilderService = ServiceLocator.Current.GetInstance<IPermissionsBuilderService>();
-			authorizationRepository = ServiceLocator.Current.GetInstance<IAuthorizationRepository>();
+            authorizationService = ServiceLocator.Current.GetInstance<IAuthorizationService>();
+            permissionService = ServiceLocator.Current.GetInstance<IPermissionsService>();
+            permissionsBuilderService = ServiceLocator.Current.GetInstance<IPermissionsBuilderService>();
+            authorizationRepository = ServiceLocator.Current.GetInstance<IAuthorizationRepository>();
 
-			authorizationRepository.CreateUsersGroup("Administrators");
-			authorizationRepository.CreateEntitiesGroup("Important Accounts");
-			authorizationRepository.CreateOperation("/Account/Edit");
-			authorizationRepository.CreateOperation("/Account/Disable");
+            authorizationRepository.CreateUsersGroup("Administrators");
+            authorizationRepository.CreateEntitiesGroup("Important Accounts");
+            authorizationRepository.CreateOperation("/Account/Edit");
+            authorizationRepository.CreateOperation("/Account/Disable");
 
-			authorizationRepository.AssociateUserWith(user, "Administrators");
-			authorizationRepository.AssociateEntityWith(account, "Important Accounts");
-		}
-	}
+            authorizationRepository.AssociateUserWith(user, "Administrators");
+            authorizationRepository.AssociateEntityWith(account, "Important Accounts");
+        }
+    }
 }
--- Rhino.Security/SecurityInfo.cs
+++ Rhino.Security/SecurityInfo.cs
@@ -11,31 +11,31 @@ namespace Rhino.Security
     public class SecurityInfo
     {
         private readonly string name;
-    	private readonly object identifier;
+        private readonly object identifier;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SecurityInfo"/> class.
-		/// </summary>
-		/// <param name="name">The name.</param>
-		/// <param name="identifier">The identifier.</param>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SecurityInfo"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="identifier">The identifier.</param>
         public SecurityInfo(string name, object identifier)
         {
             Guard.Against<ArgumentException>(string.IsNullOrEmpty(name), "Name must have a value");
             Guard.Against<ArgumentException>(identifier==null, "Identifier must not be null");
             this.name = name;
-        	this.identifier = identifier;
+            this.identifier = identifier;
         }
 
-		/// <summary>
-		/// Gets the identifier.
-		/// </summary>
-		/// <value>The identifier.</value>
-    	public virtual object Identifier
-    	{
-    		get { return identifier; }
-    	}
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public virtual object Identifier
+        {
+            get { return identifier; }
+        }
 
-    	/// <summary>
+        /// <summary>
         /// Gets the name.
         /// </summary>
         /// <value>The name.</value>
